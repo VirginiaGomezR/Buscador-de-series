ab20928af5b2c625abe942e71f40b16947da40b5
VirginiaGomezR <virginiagomezr@gmail.com>
2020-07-10 08:15:52 +0200

    js class in main.html

--- src/js/02_partial.js
+++ src/js/02_partial.js
@@ -1,6 +1,9 @@
 /*Pintar listas de HTML y escuchar evento botón*/
 
 //Pintar lista de película
+const paintList = (data) => {
+  const list = document.querySelector('.js-list');
+};
 
 //Pintar lista de películas favoritas
 
